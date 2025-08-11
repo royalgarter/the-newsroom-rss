3403d7ed14f9f938f90e9454036a20fa7faea18c
tung-ideapad <tung-ideapad@staas.io>
2025-08-11 13:08:23 +0700

    .

--- frontend/index.js
+++ frontend/index.js
@@ -631,17 +631,16 @@ function alpineRSS() { return {
 			item.description_formatted = (item.description ? this.decodeHTML(item.description) : '').substr(0, 1000);
 			item.author_formatted_short = item.author?.toString().substr(0, 12).trim();
 
-			item.loadArticle = (is_toggle) => {
+			item.loadArticle = async (is_toggle) => {
 				if (item.article?.content) {
 					item.loading = false;
-					if (is_toggle) item.read_more = !item.read_more;
 					return;
 				}
 
 				if (item.loading) return;
 
 				item.loading = true;
-				fetch(`/api/readlater?x=${this.params.x}&sig=${this?.profile?.signature || ''}&link=${item.link}`)
+				await fetch(`/api/readlater?x=${this.params.x}&sig=${this?.profile?.signature || ''}&link=${item.link}`)
 					.then(r => r.json())
 					.then(article => {
 						item.article = article;
@@ -649,7 +648,6 @@ function alpineRSS() { return {
 					.catch(e => null)
 					.finally(_ => {
 						item.loading = false;
-						if (is_toggle) item.read_more = !item.read_more;
 					})
 			}
 		});
